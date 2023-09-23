89b381e84f1f19950ea13e4e8afa3b34309a435c
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-23 13:15:19 +0330

    Rendered layout App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./Components/Layout";
+import CharacterList from "./pages/CharacterList";
+
 function App() {
-  return <div className="bg-green-500">welcome</div>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<CharacterList />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
