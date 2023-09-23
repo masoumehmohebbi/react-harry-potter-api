0dc63f6f4eb4ccca467636600bbf0657c971e756
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-23 19:31:44 +0330

    Splited search CMP Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,9 @@ import { BiHeart, BiSearch } from "react-icons/bi";
 import Select from "react-select";
 
 const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
+  { value: "characters", label: "Characters" },
+  { value: "staff", label: "Staff" },
+  { value: "vanilla", label: "Spell" },
 ];
 const selectStyles = {
   control: (base) => ({
@@ -41,38 +41,44 @@ function Header() {
         <h1 className="font-harrypotter text-4xl text-center text-white">
           Welcome to Harry Potter Universe!
         </h1>
-        {/* Search */}
-        <div className="absolute bottom-2 w-11/12 grid grid-cols-7">
-          <Select
-            className="col-span-2 mr-[1px]"
-            options={options}
-            styles={selectStyles}
-            placeholder="Select a category..."
-          />
-          <div className="col-span-5 w-full flex items-center justify-center relative">
-            <input
-              className="w-full p-2 rounded-r-md outline-none"
-              type="text"
-            />
-            <BiSearch className="absolute right-0 w-14 p-1 top-0 h-full text-white bg-red-500 rounded-r-md" />
-          </div>
-        </div>
+        <Search />
       </div>
       {/* Found result */}
       <div className="absolute left-0 top-1 pr-5 text-white">
         <p>Found X Result</p>
       </div>
-      {/* Favourites */}
-      <div className="absolute right-0 top-1 pr-5">
-        <button className="relative">
-          <BiHeart size={39} className="text-red-500" />
-          <span className="absolute top-0 -right-[6px] h-6 w-6 text-sm rounded-full bg-red-500 flex justify-center items-center">
-            0
-          </span>
-        </button>
-      </div>
+      <Favourites />
     </div>
   );
 }
 
 export default Header;
+
+function Search() {
+  return (
+    <div className="absolute bottom-2 w-11/12 grid grid-cols-7">
+      <Select
+        className="col-span-2 mr-[1px]"
+        options={options}
+        styles={selectStyles}
+        placeholder="Select a category..."
+      />
+      <div className="col-span-5 w-full flex items-center justify-center relative">
+        <input className="w-full p-2 rounded-r-md outline-none" type="text" />
+        <BiSearch className="absolute right-0 w-14 p-1 top-0 h-full text-white bg-red-500 rounded-r-md" />
+      </div>
+    </div>
+  );
+}
+function Favourites() {
+  return (
+    <div className="absolute right-0 top-1 pr-5">
+      <button className="relative">
+        <BiHeart size={39} className="text-red-500" />
+        <span className="absolute top-0 -right-[6px] h-6 w-6 text-sm rounded-full bg-red-500 flex justify-center items-center">
+          0
+        </span>
+      </button>
+    </div>
+  );
+}
