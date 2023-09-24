9f619ae97f8d088879fca04098edb674e6ed9381
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-24 10:05:20 +0330

    Added QueryProvider-Context App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom";
 
 import Layout from "./Components/Layout";
 import CharacterList from "./pages/CharacterList";
+import { QueryProvider } from "./context/QueryContext";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<CharacterList />} />
-      </Route>
-    </Routes>
+    <QueryProvider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<CharacterList />} />
+        </Route>
+      </Routes>
+    </QueryProvider>
   );
 }
 
