d30e90243bda6826221266299f2b25803e0b53c8
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-25 17:27:03 +0330

    Added UseNavigate CharacterDetails.jsx

--- src/pages/CharacterDetails.jsx
+++ src/pages/CharacterDetails.jsx
@@ -1,5 +1,26 @@
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "../Components/Header";
+
 function CharacterDetails() {
-  return <div>CharacterDetails</div>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  console.log(id);
+  return (
+    <div>
+      <Header>
+        <span></span>
+        <button onClick={() => navigate("/")} className="text-white bg-red-300">
+          Back to Home
+        </button>
+      </Header>
+      <section>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>detals...</div>
+      </section>
+    </div>
+  );
 }
 
 export default CharacterDetails;
