709637b1755ac0c78e8e4f1945ad24b98309bbc8
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-25 14:24:36 +0330

    Added IsLoading-State useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,40 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
+import toast from "react-hot-toast";
 
 function useFetch(query) {
     const [allData , setAllData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+
     useEffect(() => {
+      const controller = new AbortController()
+      const signal = controller.signal
+
           const fetchData = async () => {
-            const  data  = await axios.get(
-              `https://hp-api.onrender.com/api/${query}`
+            setIsLoading(true)
+           try {
+            const  {data}  = await axios.get(
+              `https://hp-api.onrender.com/api/${query}` , {signal}
             );
-            setAllData(data.data);
+            setAllData(data);
+
+           } catch (err) {
+          
+            toast.error(err.message)
+            if (!axios.isCancel) {
+              setAllData([])
+            
+            }
+
+           } finally{
+              setIsLoading(false)
+           }
           };
           fetchData();
         }, [query]);
 
-  return {allData}
+  return {allData , isLoading}
 }
 
 export default useFetch
