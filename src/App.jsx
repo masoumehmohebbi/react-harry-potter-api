855bea027aa254b9de47d864893984aeacb39a3c
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-25 16:08:56 +0330

    Removed Layout-render App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "./Components/Layout";
+// import Layout from "./Components/Layout";
 import CharacterList from "./pages/CharacterList";
 import { QueryProvider } from "./context/QueryContext";
+import CharacterDetails from "./pages/CharacterDetails";
 
 function App() {
   return (
     <QueryProvider>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<CharacterList />} />
-        </Route>
+        {/* <Route element={<Layout />}> */}
+        <Route path="/" element={<CharacterList />} />
+        <Route path="character/:id" element={<CharacterDetails />} />
+        {/* </Route> */}
       </Routes>
     </QueryProvider>
   );
