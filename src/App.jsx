965fb8e90c92982b0b8afa581547d0c451e5391e
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-26 11:46:56 +0330

    Added Favourites-State App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import { Route, Routes } from "react-router-dom";
 import CharacterList from "./pages/CharacterList";
 import { QueryProvider } from "./context/QueryContext";
 import CharacterDetails from "./pages/CharacterDetails";
+import { useState } from "react";
 
 function App() {
+  const [Favourites, setFavourites] = useState([]);
+  console.log(Favourites);
   return (
     <QueryProvider>
       <Routes>
-        {/* <Route element={<Layout />}> */}
-        <Route path="/" element={<CharacterList />} />
-        <Route path="character/:id" element={<CharacterDetails />} />
-        {/* </Route> */}
+        <Route
+          path="/"
+          element={
+            <CharacterList
+              Favourites={Favourites}
+              setFavourites={setFavourites}
+            />
+          }
+        />
+        <Route
+          path="character/:id"
+          element={
+            <CharacterDetails
+              Favourites={Favourites}
+              setFavourites={setFavourites}
+            />
+          }
+        />
       </Routes>
     </QueryProvider>
   );
