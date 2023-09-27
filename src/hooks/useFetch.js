cd4855d8218ffd087e5ad459549c5a7d66ca72de
masoume mohebbi <88876100+masoumehmohebbi@users.noreply.github.com>
2023-09-27 20:55:17 +0330

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -32,6 +32,9 @@ function useFetch(query,search) {
            }
           };
           fetchData();
+        return () => {
+      controller.abort();
+    };
         }, [query,search]);
 
   return {allData , isLoading}
