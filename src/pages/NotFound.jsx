bf06532fd38c1bd0a4dc0d5121d90312ffd8cc8d
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-09-27 10:46:43 +0330

    Added: NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 
 function NotFound() {
-
   return (
-   
-    <div>NotFound </div>
-  )
+    <div className="flex flex-col items-center gap-y-4">
+      <h1 className="text-xl font-border">Oh No!</h1>
+      <h2 className="font-border text-lg">Looks Like You are lost.</h2>
+      <p>
+        Luckily you are not alone. harry potter will return you to the home!
+      </p>
+      <Link to="/">
+        <button>Go Back To Home</button>
+      </Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
